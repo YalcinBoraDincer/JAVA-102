63f618be92b9c47d2af89c16b76b5aedf9b3db01
Bora <40895613+YalcinBoraDincer@users.noreply.github.com>
2022-08-26 20:35:05 +0300

    Access Modifiers

---
+++ Erisim Belirleme Public Ststic vs/erisimbelirleme/src/first/Main.java
@@ -0,0 +1,10 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+        first a=new first("Hello World !");
+
+
+
+    }
+}
---
+++ Erisim Belirleme Public Ststic vs/erisimbelirleme/src/second/second.java
@@ -0,0 +1,17 @@
+package second;
+
+import first.first;
+
+public class second {
+
+
+
+    public static void main(String[] args) {
+
+        first b=new first("Kodluyoruz ");
+        System.out.println(b.str);
+
+
+    }
+
+}
