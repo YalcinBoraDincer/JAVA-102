78ef4ff1092314abbd8be5799ea4b6d9266f3236
Bora <40895613+YalcinBoraDincer@users.noreply.github.com>
2022-12-11 14:32:36 +0300

    Add files via upload

---
+++ WrapperClases/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        String a="10";
+        int c=Integer.parseInt(a);
+        int b=20;
+        System.out.println(b+c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
